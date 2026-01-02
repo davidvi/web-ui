b824d2a355683ae1810cc2d6171899dbf421aebc
David van IJzendoorn <davidvanijzendoorn@gmail.com>
2026-01-02 22:03:32 +0100

    add password

--- webui.py
+++ webui.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()
 import argparse
+import os
 from src.webui.interface import theme_map, create_ui
 
 
@@ -9,10 +10,20 @@ def main():
     parser.add_argument("--ip", type=str, default="127.0.0.1", help="IP address to bind to")
     parser.add_argument("--port", type=int, default=7788, help="Port to listen on")
     parser.add_argument("--theme", type=str, default="Ocean", choices=theme_map.keys(), help="Theme to use for the UI")
+    parser.add_argument("--password", type=str, default=None, help="Password for web UI access (overrides WEBUI_PASSWORD env var)")
     args = parser.parse_args()
 
+    # Get password from CLI argument or environment variable
+    password = args.password or os.getenv("WEBUI_PASSWORD", "")
+    
+    # Configure authentication if password is set
+    auth = None
+    if password:
+        # Password-only authentication (empty username)
+        auth = [("", password)]
+    
     demo = create_ui(theme_name=args.theme)
-    demo.queue().launch(server_name=args.ip, server_port=args.port)
+    demo.queue().launch(server_name=args.ip, server_port=args.port, auth=auth)
 
 
 if __name__ == '__main__':
